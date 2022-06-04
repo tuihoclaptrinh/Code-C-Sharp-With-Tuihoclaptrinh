e88253139860c1546e0e3d241f9c7d7c9bc6cf93
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2022-06-04 15:34:50 +0700

    Method-fuction in C Sharp

---
+++ Method-Fuction/Method-Fuction/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Fuction
+{
+    class Calculator
+    {
+        public static void Tong(int a, int b)
+        {
+            Console.WriteLine($"Tổng {a} + {b} = {a+b}");
+        }
+
+        public static void Tong(double a, double b)
+        {
+            Console.WriteLine($"Tổng {a} + {b} = {a + b}");
+        }
+    }
+}
---
+++ Method-Fuction/Method-Fuction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace Method_Fuction
+{
+    static class Program
+    {
+        // <Access Modifier> <return type> <name_method>(<parameters>)
+        // {
+        //    Statements
+        // };
+
+        public static int Tich(this int a, int b) => a * b;
+
+        // Phương thức XinChao()
+        static void XinChao() => Console.WriteLine("Hello I'm Tuihoclaptrinh");
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode; 
+            int a = 1, b = 2;
+            double c = 3.5, d = 5.5;
+
+            XinChao();
+            Console.WriteLine(a.Tich(4));
+
+            // Gọi method từ class
+            Calculator.Tong(a, b);
+
+            // Overload method
+            Calculator.Tong(c, d);
+
+        }
+    }
+}
