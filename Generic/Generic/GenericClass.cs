70283426fdbb657e05581be4f75cd1d028f60a1f
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2022-06-08 08:16:04 +0700

    Generic in C Sharp

---
+++ Generic/Generic/GenericClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class GenericClass<T>
+    {
+        private T id;
+        public void SetID(T _id)
+        {
+            this.id = _id;
+        }
+
+        public void Infomation()
+        {
+            Console.WriteLine($"Your id is {id}");
+        }
+
+
+    }
+}
---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Info<T, S>(int age, string name)
+        {
+            Console.WriteLine($"{name} nam nay {age} tuoi");
+        }
+        static void Main(string[] args)
+        {
+            Info<int, string>(22, "Tuan Anh");
+
+            GenericClass<int> IntID = new GenericClass<int>();
+            IntID.SetID(123);
+            IntID.Infomation();
+
+            GenericClass<string> StringID = new GenericClass<string>();
+            StringID.SetID("DE140172");
+            StringID.Infomation();
+
+        }
+    }
+}
