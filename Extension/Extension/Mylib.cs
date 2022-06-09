aa03d2688b79252d196d4e222c6e873e9afac11a
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2022-06-09 11:36:35 +0700

    Extention in C Sharp

---
+++ Extension/Extension/Mylib.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public static class Mylib
+    {
+        public static double BinhPhuong(this double x) => x * x;
+        public static double Canbac2(this double x) => Math.Sqrt(x);
+        public static double Sin(this double x) => Math.Sin(x);
+        public static double Cos(this double x) => Math.Cos(x);
+
+
+    }
+}
---
+++ Extension/Extension/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static Extension.Mylib;
+
+namespace Extension
+{
+
+     static class Program
+    {
+        public static void Print(this string s, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+        static void Main(string[] args)
+        {
+            "Tuan Anh Dep Trai".Print(ConsoleColor.Red);
+
+            double x = 5;
+
+            Console.WriteLine(x.BinhPhuong());
+            Console.WriteLine(x.Canbac2());
+            Console.WriteLine(x.Cos());
+            Console.WriteLine(x.Sin());
+        }
+    }
+}
